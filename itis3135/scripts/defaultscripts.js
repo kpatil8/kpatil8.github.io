e56e9275af65ef57f2576bfd81055e5915976ca9
kpatil8 <kpatil8@uncc.edu>
2024-02-20 16:43:57 -0500

    push

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,3 +1,27 @@
 function test_script(){
     alert("this script is working.");
 }
+
+function find_polygon(){
+    let user_input = document.getElementById("favorite_number").value;
+    let number = Math.abs(Math.round(parseFloat(user_input)));
+    if (number >= 0 && number <=10){
+        const polygons=[
+        "Too few sides to be a Polygon",
+        "Too few sides to be a polygon",
+        "Line segment",
+        "Triangle",
+        "Quadrilateral",
+        "Pentagon",
+        "Hexagon",
+        "Heptagon",
+        "Octagon",
+        "Nonagon",
+        "Decagon"
+        ];
+        let polygon_name = polygons[number]
+        alert("The polygon with " + number + " sides is called a " + polygon_name);
+    } else{
+        alert("Please enter a number between 0 and 10");
+    }
+}
