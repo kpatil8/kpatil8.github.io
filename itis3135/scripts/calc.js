fb31f698380ec10eeeda9a382052513d194fa520
kpatil8 <kpatil8@uncc.edu>
2024-04-30 15:57:05 -0400

    push

--- itis3135/scripts/calc.js
+++ itis3135/scripts/calc.js
@@ -10,3 +10,27 @@ function calc() {
 function clearcalc() {
     document.getElementById('display').value = '';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.num').forEach(function(button) {
+        button.addEventListener('click', function() {
+            var value = this.getAttribute('data-value');
+            appendToDisplay(value);
+        });
+    });
+
+    document.querySelectorAll('.operator').forEach(function(button) {
+        button.addEventListener('click', function() {
+            var value = this.getAttribute('data-value');
+            appendToDisplay(value);
+        });
+    });
+
+    document.getElementById('equals').addEventListener('click', function() {
+        calc();
+    });
+
+    document.getElementById('clear').addEventListener('click', function() {
+        clearcalc();
+    });
+});
